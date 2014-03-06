2a5564a25091215baab079d993ebe9eaa1b41a45
Shuto MIkami <mkm@shuto.info>
2014-03-06 15:59:59 +0900

    ユーザーの追加処理

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -9,5 +9,14 @@ class IndexController extends AppController {
 	public function index(){
 	}
 	public function signup(){
+		if ($this->request->is('post')) {
+			$this->User->create();
+			if ($this->User->save($this->request->data)) {
+				$this->Session->setFlash(__('登録完了しました。'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('登録に失敗しました。再度お試しください。'));
+			}
+		}
 	}
 }
