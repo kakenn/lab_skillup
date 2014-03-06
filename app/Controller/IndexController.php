4de8fa06bcfabbba5f44a171ed19ba98bc35dca1
Shuto MIkami <mkm@shuto.info>
2014-03-06 11:26:08 +0900

    フォーム用にも出る設定

--- app/Controller/IndexController.php
+++ app/Controller/IndexController.php
@@ -1,13 +1,13 @@
 <?php
 class IndexController extends AppController {
+	public $used = array('User');
 	public function beforeFilter() {
 		parent::beforeFilter();
 		$this->Auth->allow(array('index','signup'));
 	}
-	public function index(){
 
+	public function index(){
 	}
 	public function signup(){
-
 	}
 }
