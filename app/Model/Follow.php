d5a43833b56b2daf1033ae7d9d0e405fc2100909
Shuto MIkami <mkm@shuto.info>
2014-03-07 11:27:55 +0900

    コメントを削除

--- app/Model/Follow.php
+++ app/Model/Follow.php
@@ -6,29 +6,12 @@ App::uses('AppModel', 'Model');
  * @property User $User
  */
 class Follow extends AppModel {
-
-/**
- * Primary key field
- *
- * @var string
- */
 	public $primaryKey = 'follow_id';
-
-/**
- * Display field
- *
- * @var string
- */
 	public $displayField = 'follow_id';
 
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
+	public function beforeSave($options = array()) {
 
-/**
- * belongsTo associations
- *
- * @var array
- */
 	public $belongsTo = array(
 		'User' => array(
 			'className' => 'User',
