4eb30f3c9a1ca0fe524b4cd3a45d9cb4a4b97789
Shuto MIkami <mkm@shuto.info>
2014-03-07 15:51:20 +0900

    コメント削除

--- app/Model/Tweet.php
+++ app/Model/Tweet.php
@@ -1,79 +1,34 @@
 <?php
 App::uses('AppModel', 'Model');
-/**
- * Tweet Model
- *
- * @property User $User
- */
 class Tweet extends AppModel {
-
-/**
- * Display field
- *
- * @var string
- */
 	public $displayField = 'user_id';
-
-/**
- * Validation rules
- *
- * @var array
- */
 	public $validate = array(
 		'text' => array(
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
 				'message' => '入力は必須です。',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 			'mb_maxLength' => array(
 				'rule' => array('mb_maxLength','140'),
 				'message' => '140文字以下にしてください。',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 		'url' => array(
 			'url' => array(
 				'rule' => array('url'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 		'created' => array(
 			'datetime' => array(
 				'rule' => array('datetime'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 			'notEmpty' => array(
 				'rule' => array('notEmpty'),
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
 	);
 
-	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
-/**
- * belongsTo associations
- *
- * @var array
- */
 	public $belongsTo = array(
 		'User' => array(
 			'className' => 'User',
